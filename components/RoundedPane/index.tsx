1941e97d1a0fa6e521b0c9b7863879abaa659965
Huy <kingbazoka@gmail.com>
2021-06-27 18:51:58 +0000

    Fix missing default value for RoundedPanel transparent

--- components/RoundedPane/index.tsx
+++ components/RoundedPane/index.tsx
@@ -1,6 +1,6 @@
 import styles from './RoundedPane.module.css';
 
-export const RoundedPanel = ({ transparent, children }) => {
+export const RoundedPanel = ({ transparent = false, children }) => {
   const classNames = transparent
     ? styles.roundedPaneTransparent
     : styles.roundedPane;
